1c73ab2f23f4e86d06acb933d9f74650ba88f5a0
chp2007 <beetle-2006@hotmail.com>
2025-02-08 13:59:49 +0800

    Update notability.js

--- notability.js
+++ notability.js
@@ -1,42 +1,28 @@
-/*************************************
+var chp2007=JSON.parse($response.body);
 
-项目名称：Notability
-下载地址：https://t.cn/A6Cgjtei
-使用声明：⚠️仅供参考，🈲转载与售卖！
-
-**************************************
-
-[rewrite_local]
-^https?:\/\/notability\.com\/(global|subscriptions) url script-response-body https://raw.githubusercontent.com/chp2007/Rewrite/main/notability.js
-
-[mitm]
-hostname = notability.com
-
-*************************************/
-
-var chp2007 = JSON.parse($response.body);
-chp2007 = {
-   "data" : {
-     "processAppleReceipt" : {
-       "error" : 0,
-       "subscription" : {
-         "productId" : "com.gingerlabs.Notability.premium_subscription",
-         "originalTransactionId" : "120002639651217",
-         "tier" : "premium",
-         "refundedDate" : null,
-         "refundedReason" : null,
-         "isInBillingRetryPeriod" : false,
-         "expirationDate" : "2099-09-09T09:09:09.000Z",
-         "gracePeriodExpiresAt" : null,
-         "overDeviceLimit" : false,
-         "expirationIntent" : null,
-         "__typename" : "AppStoreSubscription",
-         "user" : null,
-         "status" : "canceled",
-         "originalPurchaseDate" : "2022-09-09T09:09:09.000Z"
-       },
-       "__typename" : "SubscriptionResult"
+chp2007={
+  "data" : {
+    "processAppleReceipt" : {
+      "error" : 0,
+      "subscription" : {
+        "productId" : "com.gingerlabs.Notability.premium_subscription",
+        "originalTransactionId" : "120002639651217",
+        "tier" : "premium",
+        "refundedDate" : null,
+        "refundedReason" : null,
+        "isInBillingRetryPeriod" : false,
+        "expirationDate" : "2099-12-31T05:13:02.000Z",
+        "gracePeriodExpiresAt" : null,
+        "overDeviceLimit" : false,
+        "expirationIntent" : null,
+        "__typename" : "AppStoreSubscription",
+        "user" : null,
+        "status" : "active",
+        "originalPurchaseDate" : "2022-09-09T05:13:06.000Z"
+      },
+      "__typename" : "SubscriptionResult"
     }
   }
 };
-$done({body : JSON.stringify(chp2007)});
+
+$done({body:JSON.stringify(chp2007)});
