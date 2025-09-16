ab19468dc2120fe796e492774d3bf838127abf44
chp2007 <beetle-2006@hotmail.com>
2025-09-16 19:45:15 +0800

    Create gn55.js

---
+++ gn55.js
@@ -0,0 +1,68 @@
+const body = {};
+const obj = JSON.parse((typeof $response !== "undefined" && $response.body) || null);
+const gn6 = "com.goodnotes.gn6_one_time_unlock_3999";
+const gt5 = "com.goodnotes.one_time_unlock";
+const modData6 = {
+        "grace_period_expires_date" : null,
+        "plan_key" : "premium",
+        "purchase_date" : "2023-08-16T03:19:04Z",
+        "expires_date" : null
+ };
+const modData5 = {
+        "grace_period_expires_date" : null,
+        "purchase_date" : "2022-09-09T01:10:20Z",
+        "expires_date" : null
+ };
+
+if (typeof $response === "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+  body.headers = $request.headers;
+} else if (obj && obj.subscriber) {
+  obj.subscriber.non_subscriptions[gt5] = [
+        {
+          "store_transaction_id" : "320001235125468",
+          "is_sandbox" : false,
+          "ownership_type" : "PURCHASED",
+          "id" : "689fed593c3e1eb356c2c414",
+          "price" : {
+            "amount" : 50,
+            "currency" : "CNY"
+          },
+          "country" : "CN",
+          "app_type" : "ios",
+          "display_name" : "com.goodnotes.one_time_unlock",
+          "original_purchase_date" : "2022-09-09T01:10:20Z",
+          "purchase_date" : "2022-09-09T01:10:20Z",
+          "store" : "app_store"
+        }
+      ];
+  obj.subscriber.subscriptions[gn6] = {
+        "store_transaction_id" : "a3de8d234000769886gh58456cdad770",
+        "expires_date" : null,
+        "is_sandbox" : false,
+        "unsubscribe_detected_at" : null,
+        "refunded_at" : null,
+        "auto_resume_date" : null,
+        "grace_period_expires_date" : null,
+        "period_type" : "normal",
+        "purchase_date" : "2023-08-16T03:19:04Z",
+        "quotas" : {
+
+        },
+        "billing_issues_detected_at" : null,
+        "ownership_type" : "PURCHASED",
+        "app_type" : "ios",
+        "store" : "app_store",
+        "original_purchase_date" : "2023-08-16T03:19:05Z"
+  };
+  obj.subscriber.entitlements["apple_access"] = modData6;
+  obj.subscriber.entitlements["apple_access"].product_identifier = gn6;
+  obj.subscriber.entitlements["crossplatform_access"] = modData6;
+  obj.subscriber.entitlements["crossplatform_access"].product_identifier = gn6;
+  obj.subscriber.entitlements["gn5"] = modData5;
+  obj.subscriber.entitlements["gn5"].product_identifier = gt5;
+  body.body = JSON.stringify(obj);
+}
+
+$done(body);
