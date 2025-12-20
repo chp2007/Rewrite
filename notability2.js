9d7836c83cd86171a3beeb6e543ecbd087040db5
chp2007 <beetle-2006@hotmail.com>
2025-12-20 14:08:50 +0800

    Update notability2.js

--- notability2.js
+++ notability2.js
@@ -1,7 +1,44 @@
 var nb1012=JSON.parse($response.body);
-
 nb1012={
   "data" : {
+    "associateAppStoreTransactions" : {
+      "__typename" : "SubscriptionOverview",
+      "tier" : "premium",
+      "current" : {
+        "source" : "AppStoreConsumer",
+        "expirationDate" : 4100342400000,
+        "details" : {
+          "originalTransactionId" : "330001932792320",
+          "__typename" : "AppStoreSubscription",
+          "overDeviceLimit" : false,
+          "productId" : "com.gingerlabs.Notability.premium_subscription_variant",
+          "appStoreStatus" : "canceled"
+        },
+        "renewalDate" : null,
+        "tier" : "premium",
+        "gracePeriodEndDate" : null,
+        "__typename" : "Subscription"
+      },
+      "quotas" : {
+        "__typename" : "SubscriptionFeatureQuotaView",
+        "learnSummaries" : {
+          "isUsageExceeded" : false,
+          "usagePercentage" : 0,
+          "__typename" : "SubscriptionFeatureQuota"
+        },
+        "learnQuestions" : {
+          "isUsageExceeded" : false,
+          "usagePercentage" : 0,
+          "__typename" : "SubscriptionFeatureQuota"
+        },
+        "liveTranscription" : {
+          "isUsageExceeded" : false,
+          "usagePercentage" : 0,
+          "__typename" : "SubscriptionFeatureQuota"
+        }
+      },
+      "prior" : null
+    },
     "processAppleReceipt" : {
       "__typename" : "SubscriptionResult",
       "isClassic" : false,
@@ -25,5 +62,4 @@ nb1012={
     }
   }
 };
-
 $done({body:JSON.stringify(nb1012)});
